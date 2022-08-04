ef64bc8c939649a28b222e67d68a490138106939
omaran bazna <omran4d@gmail.com>
2022-08-04 12:14:44 -0400

    create the header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { selectUserEmail, selectUserName } from "./features/userSlice";
+import { useSelector } from "react-redux";
+export default function Header() {
+  const userName = useSelector(selectUserEmail());
+  const userEmail = useSelector(selectUserEmail());
+  return (
+    <div>
+      {userName ? (
+        <>
+          <div>Hello {userName}</div>
+        </>
+      ) : (
+        <>
+          <div>Log in to start the dream</div>
+        </>
+      )}
+    </div>
+  );
+}
