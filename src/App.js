8c4878c05471b219cea528c884fd61640a7d93fc
omaran bazna <omran4d@gmail.com>
2022-08-04 10:01:43 -0400

    add the google image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 import { auth, provider } from "firebase";
 import { useDispatch, useSelector } from "react-redux";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <img src="https://www.outsystems.com/forge/DownloadResource.aspx?FileName=&ImageBinaryId=43951"></img>
+    </div>
+  );
 }
 
 export default App;
