2cc811012efeca43ac2fc4aff4c70801c03d90fc
omaran bazna <omran4d@gmail.com>
2022-08-04 10:00:41 -0400

    import to the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import "./App.css";
-
+import { auth, provider } from "firebase";
+import { useDispatch, useSelector } from "react-redux";
 function App() {
   return <div></div>;
 }
