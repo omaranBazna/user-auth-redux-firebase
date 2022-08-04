b99135758abd40277c6e4b65a30eea455a0147de
omaran bazna <omran4d@gmail.com>
2022-08-04 09:37:05 -0400

    initialize the firebase app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjUCMjLUqw2xVEgbkPIhIDWuikcbUK5w8",
+  authDomain: "auth-redux-b3dd0.firebaseapp.com",
+  projectId: "auth-redux-b3dd0",
+  storageBucket: "auth-redux-b3dd0.appspot.com",
+  messagingSenderId: "672988115048",
+  appId: "1:672988115048:web:04e31bda5c7e7ae7f6cbdd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
