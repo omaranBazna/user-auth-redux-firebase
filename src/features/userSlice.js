cd2ba06e20473505e2b0cd1de15e1e279a6517fb
omaran bazna <omran4d@gmail.com>
2022-08-04 09:47:32 -0400

    export the userSlice

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -5,5 +5,8 @@ const initialState = {};
 const userSlice = createSlice({
   name: "user",
   initialState,
+  reducers: {},
 });
+
+export const {} = userSlice.actions;
 export default userSlice.reducer;
