5e33b29037d0f503c46fedc7d04bc98bd0aac9c6
omaran bazna <omran4d@gmail.com>
2022-08-04 10:23:36 -0400

    add the userReducer to reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
 import userSlice from "../features/userSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: useReducer,
+  },
 });
