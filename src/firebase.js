17cb13789798f8a1a5e0866496709c60620424ce
omaran bazna <omran4d@gmail.com>
2022-08-04 09:40:34 -0400

    initialize the auth provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, auth } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +18,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
