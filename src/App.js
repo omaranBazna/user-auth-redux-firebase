15156d844e260d80d6d09ec3bbe8f61d446b417a
omaran bazna <omran4d@gmail.com>
2022-08-04 10:04:23 -0400

    solve some issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import { auth, provider } from "firebase";
+import { auth, provider } from "./firebase";
 import { useDispatch, useSelector } from "react-redux";
 function App() {
   return (
